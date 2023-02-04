acc7e8e60d3caf99e4641a9fe4e940d3a6fc905c
Vadim <o-maestpo@yandex.ru>
2023-02-04 22:26:44 +1200

    add apiUrl

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
+import CopyPlugin from 'copy-webpack-plugin';
 
 export function buildPlugins(props: BuildOptions): WebpackPluginInstance[] {
   const { paths, isDev, apiUrl, project } = props;
@@ -21,6 +22,11 @@ export function buildPlugins(props: BuildOptions): WebpackPluginInstance[] {
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: paths.locales, to: paths.buildLocales },
+      ],
     }),
   ];
 
