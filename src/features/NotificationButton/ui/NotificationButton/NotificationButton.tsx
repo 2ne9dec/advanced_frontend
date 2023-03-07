2021b52807ac555dfcda4f1a50df821b0bd14dd9
Vadim <o-maestpo@yandex.ru>
2023-03-07 09:02:11 +0300

    add useModal hook, uprgrade modal and drawer components

--- src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,12 +1,10 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { memo, useCallback, useState } from 'react';
+import { memo } from 'react';
 import { Popover } from 'shared/ui/Popups';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Icon } from 'shared/ui/Icon/Icon';
 import { NotificationList } from 'entities/Notification';
-import NotificationIcon from 'shared/assets/icons/notification-20-20.svg';
-import { Drawer } from 'shared/ui/Drawer/Drawer';
-import { BrowserView, MobileView } from 'react-device-detect';
+import Notifications from 'shared/assets/icons/notification-20-20.svg';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -15,40 +13,23 @@ interface NotificationButtonProps {
 
 export const NotificationButton = memo((props: NotificationButtonProps) => {
   const { className } = props;
-  const [isOpen, setIsOpen] = useState(false);
-
-  const onOpenDrawer = useCallback(() => {
-    setIsOpen(true);
-  }, []);
-
-  const onCloseDrawer = useCallback(() => {
-    setIsOpen(false);
-  }, []);
-
-  const trigger = (
-    <Button onClick={onOpenDrawer} theme={ButtonTheme.CLEAR}>
-      <Icon Svg={NotificationIcon} inverted />
-    </Button>
-  );
 
   return (
-    <div>
-      <BrowserView>
-        <Popover
-          className={classNames(cls.NotificationButton, {}, [className])}
-          direction='bottom left'
-          trigger={trigger}
-        >
-          <NotificationList className={cls.notifications} />
-        </Popover>
-      </BrowserView>
-      <MobileView>
-        {trigger}
-        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-          <NotificationList />
-        </Drawer>
-      </MobileView>
-    </div>
+    <Popover
+      className={classNames(cls.NotificationButton, {}, [className])}
+      direction={'bottom left'}
+      trigger={
+        <Button theme={ButtonTheme.CLEAR}>
+          <Icon
+            Svg={Notifications}
+            inverted
+            className={cls.notification}
+          />
+        </Button>
+      }
+    >
+      <NotificationList className={cls.notifications} />
+    </Popover>
   );
 });
 
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 import { ReactNode } from 'react';
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import { useModal } from 'shared/lib/hooks/useModal/useModal';
 import { Overlay } from '../Overlay/Overlay';
+=======
+import { ReactNode, useRef, useState, useCallback, useEffect, MutableRefObject, MouseEvent } from 'react';
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
+>>>>>>> parent of 47f83dc (add drawer, add device detector)
 import { Portal } from '../Portal/Portal';
 import cls from './Modal.module.scss';
 
@@ -18,7 +23,53 @@ const ANIMATION_DELAY = 300;
 export const Modal = (props: ModalProps) => {
   const { className, children, lazy, isOpen, onClose } = props;
 
+<<<<<<< HEAD
   const { close, isClosing, isMounted } = useModal({ animationDelay: ANIMATION_DELAY, onClose, isOpen });
+=======
+  const [isClosing, setIsClosing] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
+
+  const closeHandler = useCallback(() => {
+    if (onClose) {
+      setIsClosing(true);
+      timerRef.current = setTimeout(() => {
+        onClose();
+        setIsClosing(false);
+      }, ANIMATION_DELAY);
+    }
+  }, [onClose]);
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        closeHandler();
+      }
+    },
+    [closeHandler],
+  );
+
+  const onContentClick = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsMounted(true);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', onKeyDown);
+    }
+
+    return () => {
+      clearTimeout(timerRef.current);
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [isOpen, onKeyDown]);
+>>>>>>> parent of 47f83dc (add drawer, add device detector)
 
   const mods: Mods = {
     [cls.opened]: isOpen,
@@ -32,8 +83,16 @@ export const Modal = (props: ModalProps) => {
   return (
     <Portal>
       <div className={classNames(cls.Modal, mods, [className])}>
+<<<<<<< HEAD
         <Overlay onClick={close} />
         <div className={cls.content}>{children}</div>
+=======
+        <div className={cls.overlay} onClick={closeHandler}>
+          <div className={cls.content} onClick={onContentClick}>
+            {children}
+          </div>
+        </div>
+>>>>>>> parent of 47f83dc (add drawer, add device detector)
       </div>
     </Portal>
   );
