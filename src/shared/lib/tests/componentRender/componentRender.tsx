69a757d212661c2e0149f49bf006596f42382b5b
2ne9dec <o-maestpo@yandex.ru>
2024-07-09 15:13:33 +0300

    add tests for isolated components with cypress

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,21 +5,41 @@ import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import i18nForTests from '@/shared/config/i18n/i18nForTests';
+import { Theme } from '@/shared/const/theme';
+// eslint-disable-next-line 2ne9dec-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+// eslint-disable-next-line 2ne9dec-plugin/layer-imports
+import '@/app/styles/app.scss';
 
 export interface componentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+  theme?: Theme;
 }
 
-export const componentRender = (component: ReactNode, options: componentRenderOptions = {}) => {
-  const { route = '/', initialState, asyncReducers } = options;
+interface TestProviderProps {
+  children: ReactNode;
+  options?: componentRenderOptions;
+}
 
-  return render(
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
+  const { route = '/', initialState, asyncReducers, theme = Theme.DARK } = options;
+
+  return (
     <MemoryRouter initialEntries={[route]}>
       <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
-        <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+        <I18nextProvider i18n={i18nForTests}>
+          <ThemeProvider initialTheme={theme}>
+            <div className={`app`}>{children}</div>
+          </ThemeProvider>
+        </I18nextProvider>
       </StoreProvider>
-    </MemoryRouter>,
+    </MemoryRouter>
   );
-};
+}
+
+export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+  return render(<TestProvider options={options}>{component}</TestProvider>);
+}
